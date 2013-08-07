1391db4babe975388fd72b4cc7fb35ca3c6e823b
Christian Fobel <christian@fobel.net>
2013-08-07 10:39:18 -0400

    Add Cython test case

---
+++ blitz_reshape/tests/test_reshape.py
@@ -0,0 +1,5 @@
+from .BlitzTestbed import __main__
+
+
+def test_reshape():
+    __main__()
--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import sys
+import os
+from distutils.core import setup, Extension
+
+from Cython.Build import cythonize
+import blitz as b
+
+include_dirs = [b.get_include(), b.get_cython_include()]
+sys.path += include_dirs
+
+blitz_testbed = Extension('blitz_reshape.tests.BlitzTestbed',
+                          ['blitz_reshape/tests/BlitzTestbed.pyx'],
+                          extra_objects=[os.path.join(b.get_lib(),
+                                                      'libblitz.a')],
+                          include_dirs=include_dirs, language='c++')
 
 setup(name = "blitz_reshape",
     version = "0.0.1",
@@ -11,7 +25,7 @@ setup(name = "blitz_reshape",
     license = "GPL",
     long_description = """""",
     packages = ['blitz_reshape'],
-
     package_data={'blitz_reshape': ['include/*.*', 'include/blitz/*.*',
-                                    'cython_include/*.*']}
+                                    'cython_include/*.*']},
+    ext_modules=cythonize([blitz_testbed])
 )
